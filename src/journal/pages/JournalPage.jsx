ae8c687b81e8b2e7ec3f9cab86ea08ae710abbae
unknown <marcelosebastianvaldez@gmail.com>
2024-12-08 02:34:44 -0300

    Primer commit del proyecto

---
+++ src/journal/pages/JournalPage.jsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material"
+import { MailLockOutlined } from "@mui/icons-material"
+
+export const JournalPage = () => {
+  return (
+    <>
+    <Typography variant="h1">JournalPage</Typography>
+    <MailLockOutlined />
+    </>
+  )
+}
